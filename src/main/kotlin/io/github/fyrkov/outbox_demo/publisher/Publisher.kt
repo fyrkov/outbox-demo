cfa0062c600612309e7af6f95b869ac7d5a105b8
Andrey Chirkov <fyrkov86@gmail.com>
2026-01-27 18:05:33 +0100

    replace println with logger in Publisher

--- src/main/kotlin/io/github/fyrkov/outbox_demo/publisher/Publisher.kt
+++ src/main/kotlin/io/github/fyrkov/outbox_demo/publisher/Publisher.kt
@@ -2,6 +2,7 @@ package io.github.fyrkov.outbox_demo.publisher
 
 import io.github.fyrkov.outbox_demo.domain.OutboxRecord
 import io.github.fyrkov.outbox_demo.repository.OutboxRepository
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
@@ -9,11 +10,12 @@ import org.springframework.stereotype.Component
 class Publisher(
     private val outboxRepository: OutboxRepository
 ) {
+    private val log = LoggerFactory.getLogger(javaClass)
 
     @Scheduled(fixedRateString = "\${outbox.publish.interval}")
     fun publish() {
         val records: List<OutboxRecord> = outboxRepository.selectUnpublished(100)
         // Batch publish records
-        println("Published ${records.size} records")
+        log.info("Published {} records", records.size)
     }
 }
